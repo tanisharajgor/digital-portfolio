c7b30733690c9fcee7c71d3571e6980c475e7228
Tanisha Rajgor <tanisha.rajgor@gmail.com>
2024-04-01 20:22:45 -0400

    Updated version for LinkedIn

--- src/App.js
+++ src/App.js
@@ -4,8 +4,11 @@ import Earth from "./components/earth";
 import "./styling/fonts.css";
 import Typewriter from "./components/typewriter";
 import FixedSizeWindow from "./components/fixwindowsize";
-import GitHubIcon from '@mui/icons-material/GitHub';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import "./styling/card.css";
+import "tailwindcss/tailwind.css";
+import Skills from "./components/skills";
 
 function App() {
   return (
@@ -19,8 +22,18 @@ function App() {
         </div>
       </div>
       <header className="App-header">
-        <div style={{ display: "flex", alignItems: "center", zIndex: "2" }}>
-          <Earth></Earth>
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            zIndex: "2",
+            marginTop: "10px",
+            marginLeft: "0px",
+          }}
+        >
+          <div style={{ marginLeft: "20px" }}>
+            <Earth></Earth>
+          </div>
           <div
             style={{
               flexDirection: "column",
@@ -48,36 +61,172 @@ function App() {
               style={{
                 fontSize: "18px",
                 fontFamily: "Nunito",
-                marginBottom: "10px",
+                marginBottom: "0px",
                 wordWrap: "break-word",
-                width: "40vw",
+                width: "50vw",
               }}
             >
-              Previously, I have developed solutions for Accenture and AI4ALL,
-              as well as conducted deep-learning focused research projects
+              I'm a Data Science Student at Northeastern University, with a love
+              for Machine Learning, Software Development, Mixed Reality, and
+              Astronomy.
+              <br /> <br />
+              Previously, I have developed solutions for Accenture, FM Global,
+              Boston University, and the Center for Design, as well as conducted
+              software engineering and deep-learning focused research projects
               presented at MIT, Junior Humanities Science Symposium, and the
-              National Henry Ford Invention Convention. <br /> <br />I aspire to
-              leverage analytical/statistical skills in Machine Learning and
-              Artificial Intelligence to make systems more efficient,
-              accessible, and innovative. <br /> <br /> This past summer, I
-              worked as a Data Science Intern at FM Global and a Research
-              Software Developer for the Center of Design.
+              National Henry Ford Invention Convention — focused on making
+              systems more efficient, accessible, and innovative.
+              <br /> <br />
+              I'm currently a Systems Software Developer for AerospaceNU and
+              Northeastern Electric Racing, as well as a Discrete Mathematics
+              Teaching Assistant.
+              <br /> <br />
             </p>
-            <p style={{ fontSize: "18px", fontFamily: "Nunito", display: "flex", alignItems: "center" }}>
+
+            <p
+              style={{
+                marginTop: "10px",
+                fontSize: "18px",
+                fontFamily: "Nunito",
+                display: "flex",
+                alignItems: "center",
+              }}
+            >
               Check out my:
-              <GitHubIcon style={{ marginLeft: "5px", marginRight: "5px" }}></GitHubIcon>
-              <a style={{ color: "#04D9FF", marginRight: "5px" }} href="https://github.com/tanisharajgor">Github</a>
+              <GitHubIcon
+                style={{ marginLeft: "5px", marginRight: "5px" }}
+              ></GitHubIcon>
+              <a
+                style={{ color: "#04D9FF", marginRight: "5px" }}
+                href="https://github.com/tanisharajgor"
+              >
+                Github
+              </a>
               |
-              <LinkedInIcon style={{ marginLeft: "5px", marginRight: "5px" }}></LinkedInIcon>
-              <a style={{ color: "#04D9FF", marginRight: "5px" }} href="https://www.linkedin.com/in/tanisharajgor/">LinkedIn</a>
+              <LinkedInIcon
+                style={{ marginLeft: "5px", marginRight: "5px" }}
+              ></LinkedInIcon>
+              <a
+                style={{ color: "#04D9FF", marginRight: "5px" }}
+                href="https://www.linkedin.com/in/tanisharajgor/"
+              >
+                LinkedIn
+              </a>
             </p>
           </div>
         </div>
       </header>
-      <header className="App-header" style={{ minHeight: "40vh", justifyContent: "top" }}>
-        <p style={{ marginTop: "170px", fontFamily: "Nunito", zIndex: "3" }}> What you doing? Nothing to see here! 🙈</p>
-        <p style={{ marginTop: "0px", fontFamily: "Nunito", fontSize: "12px" }}>© Copyright 2024. Made with ᡣ𐭩ྀིྀིྀི</p>
+
+      <div
+        style={{
+          justifyContent: "center",
+          alignItems: "center",
+          textAlign: "center",
+          marginTop: "20px"
+        }}
+      >
+        <p style={{ fontFamily: "Nunito", color: "white", fontSize: "30px", marginBottom: "10px" }}>
+          Tools/Technologies
+        </p>
+        <Skills></Skills>
+      </div>
+
+      <header
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          marginTop: "30px",
+        }}
+      >
+        <iframe
+          src="https://carbon.now.sh/embed?bg=rgba%2846%2C46%2C46%2C1%29&t=panda-syntax&wt=none&l=text&width=680&ds=true&dsyoff=6px&dsblur=100px&wc=true&wa=true&pv=17px&ph=23px&ln=false&fl=1&fm=Hack&fs=14px&lh=135%25&si=false&es=2x&wm=false&code=Where%2520I%27ve%2520Been%2520%25F0%259F%259A%2580%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%250A%250AWork%253A%2520%2520%2520%2520%2520%2520%2520%2520%2520%250AFM%2520Global%253A%2520Data%2520Science%2520Intern%250AAccenture%253A%2520IT%2520Consulting%2520Intern%250A%250AResearch%253A%250ACenter%2520for%2520Design%253A%2520Software%2520Developer%250ABU%2520AI4ALL%253A%2520Machine%2520Learning%2520Intern%250A%250AClubs%253A%250AElectric%2520Racing%253A%2520Software%2520Developer%250AAerospaceNU%253A%2520Systems%2520Software%2520Developer"
+          style={{
+            width: "959px",
+            height: "354px",
+            border: "0",
+            transform: "scale(1)",
+            overflow: "hidden",
+            borderRadius: "20px",
+            marginTop: "0px",
+          }}
+          sandbox="allow-scripts allow-same-origin"
+        ></iframe>
       </header>
+      <header
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          marginTop: "10px",
+        }}
+      >
+        <p
+          style={{
+            marginTop: "0px",
+            fontFamily: "Nunito",
+            fontSize: "12px",
+            color: "white",
+            padding: "10px",
+          }}
+        >
+          © Copyright Tanisha Rajgor 2024. Made with ᡣ𐭩ྀིྀིྀི
+        </p>
+      </header>
+
+      {/* <header
+        className="App-header"
+        style={{ minHeight: "40vh", justifyContent: "top" }}
+      >
+        <p style={{ fontFamily: "Nunito", zIndex: "3", fontSize: "40px" }}>
+          My Projects (Github Code, Presentations & Papers)
+        </p>
+
+        <div class="grid-backing">
+          <div class="tools">
+            <div class="circle">
+              <span class="red box"></span>
+            </div>
+            <div class="circle">
+              <span class="yellow box"></span>
+            </div>
+            <div class="circle">
+              <span class="green box"></span>
+            </div>
+          </div>
+          <div class="card__content"></div>
+          <div class="grid-container">
+            <div class="card">
+              <div class="card-info">
+                <p class="title">Magic Card</p>
+              </div>
+            </div>
+            <div class="card">
+              <div class="card-info">
+                <p class="title">Magic Card</p>
+              </div>
+            </div>
+            <div class="card">
+              <div class="card-info">
+                <p class="title">Magic Card</p>
+              </div>
+            </div>
+            <div class="card">
+              <div class="card-info">
+                <p class="title">Magic Card</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <p style={{ marginTop: "170px", fontFamily: "Nunito", zIndex: "3" }}>
+          {" "}
+          What you doing? Nothing to see here! 🙈
+        </p>
+        <p style={{ marginTop: "0px", fontFamily: "Nunito", fontSize: "12px" }}>
+          © Copyright 2024. Made with ᡣ𐭩ྀིྀིྀི
+        </p>
+      </header> */}
     </div>
   );
 }
---
+++ src/components/skills.js
@@ -0,0 +1,102 @@
+import React, { useRef, useState, useEffect } from "react";
+import {
+  SiTensorflow,
+  SiPython,
+  SiMysql,
+  SiPostgresql,
+  SiMongodb,
+  SiReact,
+  SiRedis,
+  SiDocker,
+  SiKeras,
+  SiAmazonaws,
+  SiJavascript,
+  SiCplusplus,
+  SiKotlin,
+  SiApachespark,
+} from "react-icons/si";
+import { FaJava } from "react-icons/fa6";
+import "../styling/skills.css";
+import Marquee from "react-fast-marquee";
+
+const Skills = () => {
+  return (
+    <div>
+      <Marquee>
+        <div style={{ marginTop: "10px", display: "flex", gap: "20px" }}>
+          <span className="bg-orange-100 text-orange-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-gray-800 dark:text-orange-400 border border-orange-400">
+            <SiTensorflow style={{ marginRight: "5px" }} />
+            TensorFlow
+          </span>
+
+          <span className="bg-blue-100 text-blue-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-gray-800 dark:text-blue-400 border border-blue-400">
+            <SiPython style={{ marginRight: "5px" }} />
+            Python
+          </span>
+
+          <span className="bg-red-100 text-red-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-gray-800 dark:text-red-400 border border-red-400">
+            <FaJava style={{ marginRight: "5px", transform: "scale(1.2)" }} />
+            Java
+          </span>
+
+          <span className="bg-blue-100 text-blue-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-gray-800 dark:text-blue-400 border border-blue-400">
+            <SiMysql style={{ marginRight: "5px" }} />
+            MySQL
+          </span>
+
+          <span className="bg-white-100 text-white text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-gray-800 text-white-400 border border-white-400">
+            <SiPostgresql style={{ marginRight: "5px" }} />
+            PostgreSQL
+          </span>
+
+          <span className="bg-green-100 text-green-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-gray-800 dark:text-green-400 border border-green-400">
+            <SiMongodb style={{ marginRight: "5px" }} />
+            MongoDB
+          </span>
+
+          <span className="bg-blue-500 text-blue-500 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-gray-800 dark:text-blue-300 border border-blue-300">
+            <SiReact style={{ marginRight: "5px" }} />
+            React
+          </span>
+
+          <span className="bg-blue-500 text-blue-500 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-gray-800 dark:text-yellow-300 border border-yellow-300">
+            <SiJavascript style={{ marginRight: "5px" }} />
+            JavaScript
+          </span>
+
+          <span className="bg-red-500 text-red-500 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-gray-800 dark:text-red-400 border border-red-400">
+            <SiRedis style={{ marginRight: "5px" }} />
+            Redis
+          </span>
+
+          <span className="bg-blue-500 text-blue-500 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-gray-800 dark:text-blue-400 border border-blue-400">
+            <SiDocker style={{ marginRight: "5px" }} />
+            Docker
+          </span>
+
+          <span className="bg-blue-500 text-blue-500 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-gray-800 dark:text-orange-300 border border-orange-300">
+            <SiAmazonaws style={{ marginRight: "5px" }} />
+            AWS
+          </span>
+
+          <span className="bg-blue-500 text-blue-500 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-gray-800 dark:text-blue-400 border border-blue-400">
+            <SiCplusplus style={{ marginRight: "5px" }} />
+            C++
+          </span>
+
+          <span className="bg-blue-500 text-blue-500 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-gray-800 dark:text-purple-400 border border-purple-400">
+            <SiKotlin style={{ marginRight: "5px" }} />
+            Kotlin
+          </span>
+
+          <span className="bg-blue-500 text-blue-500 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-gray-800 dark:text-orange-500 border border-orange-500">
+            <SiApachespark style={{ marginRight: "5px" }} />
+            Spark
+          </span>
+        </div>
+      </Marquee>
+    </div>
+  );
+};
+
+export default Skills;
